a70d2400ad0a00d36b13ed690ace4e761f7c1bfb
Jossy <josukattoor@gmail.com>
2023-11-06 09:24:31 +0100

    layout chnages and fix in vehicle entry

--- Program.cs
+++ Program.cs
@@ -31,12 +31,12 @@ namespace Garage_2._0
 
             app.UseAuthorization();
 
-            //app.MapControllerRoute(
-            //    name: "default",
-            //    pattern: "{controller=Home}/{action=Index}/{id?}"); 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Vehicles}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller=Vehicles}/{action=Index}/{id?}");
 
             app.Run();
         }
