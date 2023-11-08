743ba6506f5130338a934c5129750524f2f6cc56
Jossy <josukattoor@gmail.com>
2023-11-08 16:47:15 +0100

    Validation files push

---
+++ Models/Validations/ValidationAttribute.cs
@@ -0,0 +1,21 @@
+﻿
+    using System;
+    using System.ComponentModel.DataAnnotations;
+namespace Garage_2._0.Models.NewFolder
+{
+    public class ParkingStartTimeInFutureAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is DateTime parkingStartTime)
+            {
+                if (parkingStartTime <= DateTime.Now)
+                {
+                    return new ValidationResult("Parking start time must be in the future.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+
+}
