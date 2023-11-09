b833764a659b24ed65328cc09aa807623008ad53
Jossy <josukattoor@gmail.com>
2023-11-09 09:39:43 +0100

    added ignored file

---
+++ Models/RemoveVehicleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage_2._0.Models
+{
+    public class RemoveVehicleViewModel
+    {
+        [Required(ErrorMessage = "Registration number is required.")]
+        [RegularExpression(@"^[A-Za-z0-9-]+$", ErrorMessage = "Invalid registration number.")]
+        public string RegNumber { get; set; }
+    }
+
+}
